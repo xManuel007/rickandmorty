ab6b3fcc7f393c9d69db9f0f289a3b4bdb6144ab
xManuel007 <maanuu.009@gmail.com>
2024-03-17 22:27:49 -0600

    delete config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     domains: ['rickandmortyapi.com', 'media1.tenor.com', 'media3.giphy.com'], // Agrega el hostname aquí
   },
